04989c8385b673c1489a1f0792350037e0f9c4ab
guylainbaron-fr <guylain.baron@gmail.com>
2025-12-27 14:21:06 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,35 @@
+const CACHE_NAME = 'jf-aniuta-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Installation du Service Worker et mise en cache minimale
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activation et nettoyage des anciens caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Réponse aux requêtes (indispensable pour le bouton "Installer")
 self.addEventListener('fetch', (event) => {
-    // Indispensable pour l'installation sur Android
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
